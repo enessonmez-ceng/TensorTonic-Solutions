fe5500f788aa774d05846db70c98edb347afb047
Enes <152513477+enessonmez-ceng@users.noreply.github.com>
2026-08-25 22:48:34 +0300

    Solve pytorch-dropout-from-scratch

--- study-plans/pytorch-basics/pytorch-dropout-from-scratch/pytorch-dropout-from-scratch.py
+++ study-plans/pytorch-basics/pytorch-dropout-from-scratch/pytorch-dropout-from-scratch.py
@@ -3,19 +3,19 @@ import torch.nn as nn
 
 class Dropout(nn.Module):
     def __init__(self, p=0.5):
+        """
+        Returns: None
+        """
         super().__init__()
         self.p = p
+
     def forward(self, x):
         """
         Returns: tensor with dropout applied
         """
+        if not self.training or self.p == 0.0:
+            return x
         if self.p == 1.0:
             return torch.zeros_like(x)
-            
-        if self.training :
-            
-            m = (torch.rand_like(x) > self.p).to(x.dtype)
-            return torch.mul(m,x)/(1-self.p)
-        else:
-            return x
-        
+        mask = (torch.rand(x.shape) >= self.p).float()
+        return x * mask / (1 - self.p)
